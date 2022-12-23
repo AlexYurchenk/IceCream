fd1f340d3100306ab15da1dcbeea7f5aa53e4b56
Oleksii Yurchenko <yurchenkoalex13@gmail.com>
2022-12-23 21:45:04 +0200

    в

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(() => {
+    const refs = {
+        mobilMenu: document.querySelector('[data-mobil-modal]'),
+        openMobilMenuBtn: document.querySelector('[data-mobil-modal-open]'),
+        closeMobilMenuBtn: document.querySelector('[data-mobil-modal-close]'),
+        //openModalBtn: document.querySelector('[data-modal-open]'),
+        //closeModalBtn: document.querySelector('[data-modal-close]'),
+        //modal: document.querySelector('[data-modal]'),
+    };
+
+    refs.openMobilMenuBtn.addEventListener('click', toggleMobilMenu);
+    refs.closeMobilMenuBtn.addEventListener('click', toggleMobilMenu);
+    //refs.openModalBtn.addEventListener('click', toggleModal);
+    //refs.closeModalBtn.addEventListener('click', toggleModal);
+
+    function toggleMobilMenu() {
+        refs.mobilMenu.classList.toggle('is-closed');
+        document.body.classList.toggle('model-is-open');
+    }
+    //function toggleModal() {
+    //    refs.modal.classList.toggle('is-hidden');
+    //    document.body.classList.toggle('model-is-open');
+    //}
+})();
